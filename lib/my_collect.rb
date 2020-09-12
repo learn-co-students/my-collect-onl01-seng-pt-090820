af1743b140de836287cca4f25cfe68e5d33d5d0a
Pbsmith82 <pbsmith82@gmail.com>
2020-09-12 15:37:55 -0400

    collect_yield

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,20 @@
 
+def my_collect(my_array)
+    item_index = 0
+    my_collection = []
+    while item_index < my_array.length
+        my_collection << yield(my_array[item_index])
+        item_index += 1
+    end
+    my_collection
+end
 
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
+
+collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end
