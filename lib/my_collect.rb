92afe300b870303fe496b7a715a9c2288388cd62
Alan Young <alanryoung01@gmail.com>
2020-09-16 09:46:33 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
+collection = ['ruby', 'javascript', 'python', 'objective-c']
 
-
+def my_collect(array)
+  i = 0
+  languages = []
+  while i < array.length
+    languages.push yield(array[i])
+    i += 1
+  end
+  languages
+end
