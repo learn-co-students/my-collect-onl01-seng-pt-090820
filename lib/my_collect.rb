c4c50129bb25d55e650ab88bf9d394f527e3ac6a
thoughtful-storage-6584 <csimon1127@gmail.com>
2020-09-18 20:10:54 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
+def my_collect(collection)
+  counter = 0
+  temp_col = []
+  while counter < collection.length
+    temp_col << collection[counter]
+    temp_col[counter] = yield(collection[counter])
+    counter += 1
+  end
+  temp_col
+end
 
