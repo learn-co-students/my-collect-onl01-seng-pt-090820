152e24b50a1b6670f9cc679c6ca070a17d6c3027
tmurrell2020 <littlerell12@gmail.com>
2020-09-20 10:23:12 -0400

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,16 @@
+require 'pry'
 
+def my_collect(array)
+    list = 0
+    languages = []
+    students = []
+    while list < array.length && array 
+        languages << yield(array[list].upcase)
+        students << yield(array[list].split(" ").first)
+        list += 1
+    
+    end
+    languages
+    students
+end
 
